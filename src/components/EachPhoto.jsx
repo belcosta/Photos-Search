33ab94656e51bc80c4a993efe96d6889a776a3f4
belccarvalho <belccarvalho@gmail.com>
2021-02-27 16:28:36 +0100

    display links

--- src/components/EachPhoto.jsx
+++ src/components/EachPhoto.jsx
@@ -3,17 +3,18 @@ import React from "react";
 export default function EachPhoto(props) {
   return (
     <div>
-      <div className='card'>
+      <div className="card">
         <img
-          className='card-img-top'
+          className="card-img-top"
           src={props.photo.cover_photo.urls.regular}
-          alt='Card pic'
+          alt="Card pic"
         />
-        <div className='card-body'>
-          <h5 className='card-title'>
+        <div className="card-body">
+          <h5 className="card-title">
             {props.photo.cover_photo.alt_description || props.photo.title}
           </h5>
         </div>
+        <div></div>
       </div>
     </div>
   );
